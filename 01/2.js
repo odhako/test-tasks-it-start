9339ff4bef4a1af529d1a473b7e68f42e95d9a89
odhako <odhako@gmail.com>
2023-02-26 11:41:11 +0400

    1.2

---
+++ 01/2.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return BigInt(a) + BigInt(b);
+}
+
+function multiply(a, b) {
+  return BigInt(a) * BigInt(b);
+}
+
+function subtract(a, b) {
+  return BigInt(a) - BigInt(b);
+}
+
+function divide(a, b) {
+  return BigInt(a) / BigInt(b);
+}
